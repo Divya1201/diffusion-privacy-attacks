547a2990c6e50bdb844274ec1c3732c8e5780f71
Divya1201 <75245503+Divya1201@users.noreply.github.com>
2026-03-30 13:51:55 +0530

    Update visualize.py

--- src/diffusion_privacy_attacks/visualize.py
+++ src/diffusion_privacy_attacks/visualize.py
@@ -1,1 +1,21 @@
+import matplotlib.pyplot as plt
+from PIL import Image
 
+
+def show_pair(gen_path, ref_path):
+    gen = Image.open(gen_path)
+    ref = Image.open(ref_path)
+
+    plt.figure(figsize=(8, 4))
+
+    plt.subplot(1, 2, 1)
+    plt.imshow(gen)
+    plt.title("Generated")
+    plt.axis("off")
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(ref)
+    plt.title("Match")
+    plt.axis("off")
+
+    plt.show()
