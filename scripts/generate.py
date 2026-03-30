c3b1a4b2ce89c4839f1c7a7d373660c9b102e38c
Divya1201 <75245503+Divya1201@users.noreply.github.com>
2026-03-30 12:29:53 +0530

    Update generate.py

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,1 +1,12 @@
+from diffusion_privacy_attacks.diffusion import load_model
+import os
 
+pipe = load_model()
+
+prompt = "a photo of a dog"
+
+os.makedirs("generated", exist_ok=True)
+
+for i in range(100):   # start with 100 (later 500)
+    image = pipe(prompt).images[0]
+    image.save(f"generated/img_{i}.png")
